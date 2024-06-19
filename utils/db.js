7b2b5ce21186d7bb600859649d83e81986f5b330
unkown007 <victormatusse00@gmail.com>
2024-06-19 20:24:02 +0200

    added file

---
+++ utils/db.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${this.host}:${this.port}/${this.database}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
