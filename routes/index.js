359fa65bfb89e8d755020fc2b9efa8864d5c6588
unkown007 <victormatusse00@gmail.com>
2024-06-19 21:28:55 +0200

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-import { Express } from 'express';
 import AppController from '../controllers/AppController';
 
-
 const Routes = (api) => {
-    api.get('/status', AppController.getStatus);
-    api.get('/stats', AppController.getStats);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
 };
 
 export default Routes;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
 import Routes from './routes';
 
+const express = require('express');
+
 const server = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 Routes(server);
 
